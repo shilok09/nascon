3d33c27581699035506e93f2f57ecf0b41d7d13c
shilokKumar <shilukr3@gmail.com>
2025-04-30 22:43:39 +0500

    done dashboard

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('index', {
+    res.render('dashboard', {
         title: 'NaSCon - National Solutions Convention',
         stats: {
             attendees: '5k+',
